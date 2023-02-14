2341caa26c898a91e183bbc6923ab6b3b257d4f1
Thon Ly <thonly@heartbank.ceo>
2023-02-14 09:13:26 -0800

    begin v1

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { THON } from "https://i.thonly.org/global.mjs";
+
+window.onload = async () => {
+    await import(`${THON}/components/tl-account/element.mjs`);
+    await import(`${THON}/components/tl-actions/element.mjs`);
+    await import(`${THON}/components/tl-stocks/element.mjs`);
+    await import(`${THON}/components/tl-provider/element.mjs`);
+};
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
